d3aaa52a3fff8f59ebcbdaf138e878326dcef427
wasim-dev <wasimahmad@confianzaitsolutions.com>
2024-09-26 12:54:55 +0530

    some fixes

---
+++ Presentation/Nop.Web/Components/HomePageManufacturesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components;
+
+public partial class HomePageManufacturesViewComponent : NopViewComponent
+{
+    protected readonly ICatalogModelFactory _catalogModelFactory;
+
+    public HomePageManufacturesViewComponent(ICatalogModelFactory catalogModelFactory)
+    {
+        _catalogModelFactory = catalogModelFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var model = await _catalogModelFactory.PrepareManufacturerAllModelsAsync();
+        if (!model.Any())
+            return Content("");
+
+        return View(model);
+    }
+}
